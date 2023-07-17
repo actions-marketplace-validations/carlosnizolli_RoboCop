6a7e194d95324d5c2520f5fa432c919e12eb5bc5
Carlos Nizolli <55762776+carlosnizolli@users.noreply.github.com>
2023-07-16 22:04:44 -0300

    Update robocop.sh

--- robocop.sh
+++ robocop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
-robocop $2 --configure return_status:quality_gates:E=0:W=$1:I=-1 --argumentfile $3
+if [ -n $3 ]
+then
+  robocop $2 --configure return_status:quality_gates:E=0:W=$1:I=-1 --argumentfile $3
+else
+  robocop $2 --configure return_status:quality_gates:E=0:W=$1:I=-1
